182e587ba417ae8a51639f3fb0d4674465a7feb8
Sufi-to <3thomas225@gmail.com>
2024-08-28 23:59:09 +0400

    type annotation

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,9 +2,10 @@
 """Module for filtering data and logging"""
 
 import re
+from typing import List
 
 
-def filter_datum(fields: list[str], redaction: str,
+def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """Returns the log message obfuscated:"""
     pattern = r"|".join([f"(?<={field}=)[^{separator}]+" for field in fields])
