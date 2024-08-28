b5659c86f1879557db3c3e873a0bf84db53e9b46
Sufi-to <3thomas225@gmail.com>
2024-08-29 03:50:45 +0400

    format the log record

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,7 @@
 """Module for filtering data and logging"""
 
 import re
+import logging
 from typing import List
 
 
@@ -10,3 +11,20 @@ def filter_datum(fields: List[str], redaction: str,
     """Returns the log message obfuscated:"""
     pattern = r"|".join([f"(?<={field}=)[^{separator}]+" for field in fields])
     return re.sub(pattern, redaction, message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Formats a log records"""
+        log = super(RedactingFormatter, self).format(record=record)
+        return filter_datum(self.fields, self.REDACTION, log, self.SEPARATOR)
