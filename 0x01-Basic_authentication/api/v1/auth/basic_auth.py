8078d89a15a759577925ce0e01afa6e35e579a51
Sufi-to <3thomas225@gmail.com>
2024-09-03 07:06:23 +0400

    fixed pycodestyle

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,7 +10,7 @@ class BasicAuth(Auth):
     def extract_base64_authorization_header(self,
                                             authorization_header: str) -> str:
         """Extract the base64 string that is needed to be decoded"""
-        if authorization_header is None or type(authorization_header) is not str:
+        if authorization_header is None or type(authorization_header) != str:
             return None
         auth_head = authorization_header.strip()
         if auth_head[0:6] != 'Basic ':
